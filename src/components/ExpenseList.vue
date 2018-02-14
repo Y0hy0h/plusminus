d11ffc382933047a10ba4728b4a664a09b73fba3
Y0hy0h <Y0hy0h@users.noreply.github.com>
2018-02-14 14:04:49 +0100

    Style expense list in Material design

--- src/components/ExpenseList.vue
+++ src/components/ExpenseList.vue
@@ -1,7 +1,12 @@
 <template>
-    <ul class="expense-list">
-        <li v-for="expense in allExpenses">{{ expense.cents }} | {{ expense.description }}</li>
-    </ul>
+    <v-list two-line class="expense-list">
+        <v-list-tile v-for="expense in allExpenses">
+            <v-list-tile-content>
+                <v-list-tile-title>{{ expense.cents }}</v-list-tile-title>
+                <v-list-tile-sub-title>{{ expense.description }}</v-list-tile-sub-title>
+            </v-list-tile-content>
+        </v-list-tile>
+    </v-list>
 </template>
 
 <script lang="ts">
