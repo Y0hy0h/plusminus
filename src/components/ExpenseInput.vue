c9023e9febbc6a2f2010cda136bd70f4bdbd65d5
Y0hy0h <Y0hy0h@users.noreply.github.com>
2018-02-16 16:08:18 +0100

    Fix currency and date formatting

--- src/components/ExpenseInput.vue
+++ src/components/ExpenseInput.vue
@@ -67,4 +67,6 @@
 </script>
 
 <style scoped lang="stylus">
+    .amount /deep/ input
+        text-align: right
 </style>
--- src/components/ExpenseList.vue
+++ src/components/ExpenseList.vue
@@ -2,7 +2,7 @@
     <v-list two-line class="expense-list">
         <v-list-tile v-for="expense in allExpenses" :key="expense.cents">
             <v-list-tile-content>
-                <v-list-tile-title>{{ expense.cents }}</v-list-tile-title>
+                <v-list-tile-title>{{ expense.cents | currency }}</v-list-tile-title>
                 <v-list-tile-sub-title>{{ dateString(expense.date) }} | {{ expense.description }}
                 </v-list-tile-sub-title>
             </v-list-tile-content>
@@ -20,7 +20,7 @@
 
     public dateString(date: Date) {
       const formatOptions = {day: '2-digit', month: '2-digit', year: 'numeric'}
-      return date.toLocaleDateString(undefined, formatOptions)
+      return date.toLocaleDateString('de-DE', formatOptions)
     }
   }
 </script>
