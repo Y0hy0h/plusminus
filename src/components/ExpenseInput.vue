e9ca1cf230051243500f3c8a484ae4562faa0de9
Y0hy0h <Y0hy0h@users.noreply.github.com>
2018-02-20 18:56:21 +0100

    Fix date picker

--- src/components/ExpenseInput.vue
+++ src/components/ExpenseInput.vue
@@ -18,11 +18,14 @@
                 />
                 <v-menu
                         ref="menu"
+                        v-model="dateMenu"
+                        lazy
                         min-width="290px"
                         full-width
                         offset-y
                         :nudge-right="40"
                         :close-on-content-click="false"
+                        :return-value.sync="dateString"
                 >
                     <v-text-field
                             slot="activator"
@@ -33,7 +36,7 @@
                     />
                     <v-date-picker v-model="dateString" no-title scrollable>
                         <v-spacer></v-spacer>
-                        <v-btn flat color="primary" @click="menu = false">Cancel</v-btn>
+                        <v-btn flat color="primary" @click="dateMenu = false">Cancel</v-btn>
                         <v-btn flat color="primary" @click="$refs.menu.save(dateString)">OK</v-btn>
                     </v-date-picker>
                 </v-menu>
@@ -52,6 +55,8 @@
   @Component
   export default class ExpenseInput extends Vue {
     @Prop() private expenseToUpdate!: Expense | null
+
+    private dateMenu: boolean = false
 
     get expense(): Expense {
       if (this.expenseToUpdate === null) {
