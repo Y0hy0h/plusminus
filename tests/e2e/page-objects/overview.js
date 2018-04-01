c0b35ee6053f230467188ddf5ad04ab09ff7deee
Y0hy0h <Y0hy0h@users.noreply.github.com>
2018-04-01 21:11:44 +0200

    Fix E2E test

--- tests/e2e/page-objects/overview.js
+++ tests/e2e/page-objects/overview.js
@@ -1,8 +1,8 @@
 module.exports = {
   url () { return process.env.VUE_DEV_SERVER_URL },
   elements: {
-    newExpenseAmount: '.amount input',
-    newExpenseDescription: '.description input',
+    newExpenseAmount: '.amount',
+    newExpenseDescription: '.description',
     saveButton: 'button.save',
     expenseList: '.expense-list',
   }
